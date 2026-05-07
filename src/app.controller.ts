896e5e2d4d1608697c086d6d6f0e3f9387395afc
Charles <charlescong91@gmail.com>
2026-05-07 21:22:09 +0700

    chore/setup (#1)
    * chore: add dependencies
    
    * feat: add swagger documentation
    
    * feat: add middlewares
    
    * feat: add database schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, BadRequestException } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('test-success')
+  getSuccess() {
+    return {
+      id: 1,
+      name: 'John Doe',
+      role: 'Admin',
+    };
+  }
+  @Get('test-error-string')
+  getErrorString() {
+    throw new BadRequestException('Email sudah terdaftar, gunakan email lain');
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('test-error-array')
+  getErrorArray() {
+    throw new BadRequestException([
+      'password must be longer than or equal to 8 characters',
+      'password must contain an uppercase letter',
+    ]);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('HapHap API')
+    .setDescription('Documentation for HapHap API')
+    .setVersion('0.0')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
