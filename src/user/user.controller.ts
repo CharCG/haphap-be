8dc20d1f1a93f1a9e576f2611e8b5639c9175d3b
Charles <charlescong91@gmail.com>
2026-05-10 00:19:41 +0700

    feat/users (#7)
    * feat: add users endpoints
    
    * feat: add can change phone number

---
+++ src/user/user.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'generated/prisma/enums';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+@Controller('users')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.CUSTOMER, Role.MERCHANT)
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  async getMe(@CurrentUser() user: any) {
+    return this.userService.getMe(user.userId);
+  }
+
+  @Patch('me')
+  async updateMe(@CurrentUser() user: any, @Body() dto: UpdateUserDto) {
+    return this.userService.updateMe(user.userId, dto);
+  }
+
+  @Patch('me/password')
+  async updatePassword(
+    @CurrentUser() user: any,
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    return this.userService.updatePassword(user.userId, dto);
+  }
+}
