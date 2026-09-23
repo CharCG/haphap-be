1aa50a805bfa30b0d7eac139dd0254937c5d90e3
Charles <charlescong91@gmail.com>
2026-09-23 20:51:23 +0700

    fix: remove allowed origins

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    allowedOrigins: process.env.ALLOWED_ORIGINS!.split(',').map((o) => o.trim()),
-    credentials: true,
-  });
+  app.enableCors({ credentials: true });
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
